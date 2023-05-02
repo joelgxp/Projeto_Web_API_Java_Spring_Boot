7dbf366fd6b5d77b5b4dd93e51c721ae24eee15f
Joel <joelvieirasouza@gmail.com>
2023-05-02 13:31:35 -0300

    Alteracoes no front

--- back/src/main/java/Projeto/API/controller/UsuarioController.java
+++ back/src/main/java/Projeto/API/controller/UsuarioController.java
@@ -3,17 +3,39 @@ package Projeto.API.controller;
 import Projeto.API.DAO.IUsuario;
 import Projeto.API.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@CrossOrigin("*")
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
     private IUsuario dao;
-    @GetMapping("/usuarios")
+    @GetMapping
     public List<Usuario> listaUsuarios() {
         return (List<Usuario>) dao.findAll();
+    }
+
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @PutMapping
+    public Usuario editarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Usuario> deleteUsuario(@PathVariable Integer id) {
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
     }
 }
--- front/script.js
+++ front/script.js
@@ -1,41 +1,40 @@
 const formulario = document.querySelector("form");
-const Inome = document.querySelector(".nome")
-const Iemail = document.querySelector(".email")
-const Isenha = document.querySelector(".senha")
-const Itel = document.querySelector(".tel")
+const Inome = document.querySelector(".nome");
+const Iemail = document.querySelector(".email");
+const Isenha = document.querySelector(".senha");
+const Itel = document.querySelector(".tel");
 
 function cadastrar() {
-    fetch("http://localhost:8080/cadastrar",
-    {
-        headers: {
-            'Accept': 'application/json',
-            'Contente-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify({
-            nome: Inomne.value,
-            email: Iemail.value,
-            senha: Isenha.value,
-            telefone: Itel.value
-        })
-    })
+  fetch("http://localhost:8080/usuarios", {
+    headers: {
+      Accept: "application/json",
+      "Contente-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      nome: Inome.value,
+      email: Iemail.value,
+      senha: Isenha.value,
+      telefone: Itel.value,
+    }),
+  })
     .then(function (res) {
-        console.log(res)
+      console.log(res);
     })
     .catch(function (res) {
-        console.log(res)
-    })
-};
+      console.log(res);
+    });
+}
 
 function limpar() {
-    nome: Inomne.value ="";
-            Iemail.value ="";
-            Isenha.value ="";
-            Itel.value ="";
-};
+  Inome.value = "";
+  Iemail.value = "";
+  Isenha.value = "";
+  Itel.value = "";
+}
 
-formulario.addEventListener('submit', function (event) {
-    event.preventDefault();
-    cadastrar(); 
-    limpar();
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  cadastrar();
+  limpar();
 });
